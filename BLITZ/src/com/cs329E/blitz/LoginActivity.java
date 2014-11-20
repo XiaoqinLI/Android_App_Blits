27c1ee9f3f94d349ef07a23a67043c4e76b99216
XiaoqinLI <daybreaklee@utexas.edu>
2014-11-20 14:19:54 +0800

    set up login, register, Bliz selection activity and layout, layout works fine on phone but not perfect on tablet

--- BLITZ/src/com/cs329E/blitz/LoginActivity.java
+++ BLITZ/src/com/cs329E/blitz/LoginActivity.java
@@ -1,36 +1,149 @@
 package com.cs329E.blitz;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+	private static final String TAG = "LoginActivity";
+	private static final int GET_TEXT_REQUEST_CODE = 1;
+	private EditText usernameEdit;
+	private EditText passwordEdit;
+		
+	private void signIn(){
+		usernameEdit = (EditText) findViewById(R.id.usernameText);
+		passwordEdit = (EditText) findViewById(R.id.passwordText);		
+		String username = usernameEdit.getText().toString();
+		String password = passwordEdit.getText().toString();
+		SharedPreferences myPrefs = this.getSharedPreferences("myPrefs", MODE_PRIVATE);
+		SharedPreferences.Editor editor = myPrefs.edit();
+		editor.putString("username", username);
+		editor.putString("password", password);
+		editor.commit();
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Log.i(TAG, "created the login activity");
+		
+		usernameEdit = (EditText) findViewById(R.id.usernameText);
+		passwordEdit = (EditText) findViewById(R.id.passwordText);
+		
+		SharedPreferences myPrefs = this.getSharedPreferences("myPrefs", MODE_PRIVATE);//  Preferences(Context.MODE_PRIVATE);
+        String storedUsername = myPrefs.getString("username", "");
+        String storedPassword = myPrefs.getString("password", "");
+       
+		
+        if ((storedUsername != null && !storedUsername.isEmpty()) && (storedPassword != null && !storedPassword.isEmpty()))
+        {
+        	Log.v(TAG, "Saved username and password detected. Launching game selection screen");
+			Intent explicitIntent = new Intent(LoginActivity.this, BlitzSelectionActivity.class);
+			startActivity(explicitIntent);
         }
-        return super.onOptionsItemSelected(item);
+		       
+		
+		final Button registerButton = (Button) findViewById(R.id.registerButton);		
+		registerButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Log.v(TAG, "User pressed the register button");
+				Intent explicitIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+				startActivityForResult(explicitIntent, GET_TEXT_REQUEST_CODE);
+			}
+		});
+		
+		final Button loginButton = (Button) findViewById(R.id.loginButton);		
+		loginButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {				
+				Log.v(TAG, "User pressed the login button");
+				if (usernameEdit.getText().toString().length() != 0 && passwordEdit.getText().toString().length() != 0){	
+					signIn();
+					Intent loginIntent = new Intent(LoginActivity.this, BlitzSelectionActivity.class);
+					startActivity(loginIntent);			
+				}
+				else{
+					Toast toast = new Toast(getApplicationContext());
+
+			        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+			        toast.setDuration(Toast.LENGTH_LONG);
+			        
+			        toast.setView(getLayoutInflater().inflate(R.layout.custom_toast,null));
+
+			        toast.show();
+				}
+			}
+		});
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        
+		Log.i(TAG, "Entered onActivityResult()");
+		
+		// If so, update the username editText showing the user-entered username.	
+		if (requestCode == GET_TEXT_REQUEST_CODE){
+			if (resultCode == RESULT_OK){
+				usernameEdit.setText(data.getStringExtra("Explicit_Activity"));
+			}
+		}   
     }
+
+	@Override
+	protected void onStart() {
+		Log.i(TAG, "started the login activity");
+		super.onStart();
+	}
+
+	@Override
+	protected void onRestart() {
+		Log.i(TAG, "restarted the login activity");
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		Log.i(TAG, "resumed the login activity");
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		Log.i(TAG, "pause the login activity");
+		super.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		Log.i(TAG, "stopped the login activity");
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		Log.i(TAG, "destroyed the login activity");
+		super.onDestroy();
+	}
+
+	/*
+	 * @Override public boolean onCreateOptionsMenu(Menu menu) { // Inflate the
+	 * menu; this adds items to the action bar if it is present.
+	 * getMenuInflater().inflate(R.menu.login, menu); return true; }
+	 * 
+	 * @Override public boolean onOptionsItemSelected(MenuItem item) { // Handle
+	 * action bar item clicks here. The action bar will // automatically handle
+	 * clicks on the Home/Up button, so long // as you specify a parent activity
+	 * in AndroidManifest.xml. int id = item.getItemId(); if (id ==
+	 * R.id.action_settings) { return true; } return
+	 * super.onOptionsItemSelected(item); }
+	 */
 }
---
+++ BLITZ/src/com/cs329E/blitz/RegisterActivity.java
@@ -0,0 +1,86 @@
+package com.cs329E.blitz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends Activity {
+
+	private static final String TAG = "registeractivity";
+	private EditText usernameEdit;
+	private EditText passwordEdit;
+	private EditText confirmPasswordEdit;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+		usernameEdit = (EditText) findViewById(R.id.usernameText);
+		passwordEdit = (EditText) findViewById(R.id.passwordText);
+		confirmPasswordEdit = (EditText) findViewById(R.id.passwordConfirmText);
+		
+		final Button registrationButton = (Button) findViewById(R.id.register_user_button);
+		registrationButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {			
+				Log.v(TAG, "closing the register screen");
+				signupClicked();
+			}
+		});	
+	}
+	
+	private void signupClicked() {
+
+		Log.i(TAG,"Entered signupClicked()");		
+		String username = usernameEdit.getText().toString();
+		String password = passwordEdit.getText().toString();
+		String confirmPassword = confirmPasswordEdit.getText().toString();
+		
+		if (password.equals(confirmPassword) && password.length() != 0 && username.length() != 0){
+			Log.i(TAG,"Entered Registration Succeeded");
+			
+			Intent explicitCallbackIntent = new Intent(this, LoginActivity.class);
+			explicitCallbackIntent.putExtra("Explicit_Activity", username);// (String name, String value)
+			setResult(RESULT_OK, explicitCallbackIntent);
+			this.finish();
+		}
+		else{
+			Toast toast = new Toast(getApplicationContext());
+
+	        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+	        toast.setDuration(Toast.LENGTH_LONG);
+	        
+	        toast.setView(getLayoutInflater().inflate(R.layout.custom_toast,null));
+
+	        toast.show();
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
